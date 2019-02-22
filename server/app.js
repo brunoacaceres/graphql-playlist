7362f6f510e72369f38cff76fc5ac4b09efcd5d1
Bruno Caceres <brunocaceres@MacBook-Pro-de-Bruno.local>
2019-02-22 06:09:30 -0400

    RootQuery.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const graphqllHTTP = require('express-graphql');
 const express = require('express');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqllHTTP({
-    
+    schema
 }))
 
 app.listen(4000,() =>{
