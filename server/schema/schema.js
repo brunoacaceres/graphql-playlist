1c9c2f94c02c0029fec3cd05f2fb6461ae4ccab0
Bruno Caceres <brunocaceres@MacBook-Pro-de-Bruno.local>
2019-02-24 22:08:36 -0400

    Change ID String to GraphQLID.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,12 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID
+} = graphql;
 
 // Dummy data
 var books = [
@@ -12,7 +17,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields:() => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
@@ -23,9 +28,10 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args:{id:{ type: GraphQLString}},
+            args:{id:{ type: GraphQLID}},
             resolve(parent, args){
                 // Code to get data from db / other source.
+                console.log(typeof(args.id));
                 return _.find(books,{id: args.id});
             }
         }
