4684aaeebb246e786211d8e2f9382bcca10d3591
Bruno Caceres <brunocaceres@MacBook-Pro-de-Bruno.local>
2019-02-24 22:31:50 -0400

    Type Relations.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 // Dummy data
 var books = [
-    {name: 'Name of the world', genre: 'Fantasy', id: '1'},
-    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-    {name: 'The long Earth', genre: 'Sci-Fi', id: '3'},
+    {name: 'Name of the world', genre: 'Fantasy', id: '1', authorId: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+    {name: 'The long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
 ];
 
 var authors = [
@@ -27,7 +27,14 @@ const BookType = new GraphQLObjectType({
     fields:() => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                console.log(parent);
+                return _.find(authors,{id:parent.authorId});
+            }
+        }
     })
 });
 
