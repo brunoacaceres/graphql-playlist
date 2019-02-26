f6f33dfde5a130a43542d580133eab7146e0127d
Bruno Caceres <brunocaceres@MacBook-Pro-de-Bruno.local>
2019-02-25 22:11:48 -0400

    Connection to MongoDB.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const graphqlHTTP = require('express-graphql');
 const express = require('express');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://mongo:senha123@ds151805.mlab.com:51805/gql-ninja');
+mongoose.connection.once('open', () =>{
+    console.log('connected to database');
+});
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
